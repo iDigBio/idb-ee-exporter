4857ccfa0ded6b36d02883b467bf1166c14d7ee6
Matthew Collins <mcollins@acis.ufl.edu>
2018-06-25 20:23:14 -0400

    Changed setup to run on okapi w/ big mem to be able to do sort

--- mk_test_data.py
+++ mk_test_data.py
@@ -6,12 +6,12 @@ import pyspark.sql.types as types
 sc = SparkContext(appName="iDigBioParquet")
 sqlContext = SQLContext(sc)
 
-(sqlContext.read.parquet("/guoda/data/idigbio-20180414T023309.parquet")
+(sqlContext.read.parquet("data/idigbio-20180526T023311.parquet")
     .orderBy("uuid")
     .limit(1000 * 1000)
     .write
     .mode("overwrite")
-    .parquet("/tmp/idigbio-20180414T023309-sorted1M.parquet")
+    .parquet("data/idigbio-20180526T023311-1Msorted.parquet")
 )
 
 
--- mk_test_data_run.sh
+++ mk_test_data_run.sh
@@ -4,10 +4,12 @@ export HADOOP_CONF_DIR="/etc/hadoop/conf"
 export HADOOP_USER_NAME="hdfs"
 
 /opt/spark/latest/bin/spark-submit \
-             --master mesos://zk://mesos02:2181,mesos01:2181,mesos03:2181/mesos \
-             --driver-memory 10G \
+             --driver-memory 256G \
              --total-executor-cores 120 \
-             --executor-memory 20G \
+             --conf spark.local.dir=/dev/shm \
              mk_test_data.py
 
 #             --conf spark.sql.shuffle.partitions=3000 \
+#             --master mesos://zk://mesos02:2181,mesos01:2181,mesos03:2181/mesos \
+#              --executor-memory 20G \
+ 
